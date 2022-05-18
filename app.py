117e4b70472ba1344cdf8f81335a8d27b1bf9080
sayrilkun <34339698+sayrilkun@users.noreply.github.com>
2022-05-18 21:56:41 +0800

    logos

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import streamlit as st  # pip install streamlit
 from openpyxl import load_workbook
 import numpy as np
 from sklearn.linear_model import LinearRegression
+from PIL import Image
 
 # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
 st.set_page_config(page_title="Median Lethal Dose Calculator", page_icon=":seedling:", layout="wide")
@@ -82,7 +83,6 @@ def lr():
     st.subheader("🚧 Final Decision")
     st.markdown("##")
 
-    from PIL import Image
     image = Image.open('table.png')
 
     st.image(image, caption='R-Squared Interpretation')
@@ -97,10 +97,17 @@ data = pd.read_csv('probit.csv')
 pTable_df = pd.DataFrame(data)
     
 # ---- SIDEBAR ----
-st.sidebar.header("Please Choose a file first")
+side_left, side_right = st.sidebar.columns(2)
+
+pnri = Image.open('pnri-4.png')
+st.sidebar.image(pnri)
+st.sidebar.header("Philippine Nuclear Reasearch Institute")
+    # st.write("Philippine Nuclear Reasearch Institute")
+
 
 mort_list = []
 dose = []
+
 
 uploaded_file = st.sidebar.file_uploader("Choose a file")
 # if uploaded_file is not None:
@@ -263,130 +270,13 @@ if st.sidebar.button("Calculate LD50"):
     lr()
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# st.write('The current movie title is', title)
-
-
-# city = st.sidebar.multiselect(
-#     "Select the Ciefty:",
-#     options=df["City"].unique(),
-#     default=df["City"].unique()
-# )
-
-# customer_type = st.sidebar.multiselect(
-#     "Select the Customer Type:",
-#     options=df["Customer_type"].unique(),
-#     default=df["Customer_type"].unique(),
-# )
-
-# gender = st.sidebar.multiselect(
-#     "Select the Gender:",
-#     options=df["Gender"].unique(),
-#     default=df["Gender"].unique()
-# )
-
-# df_selection = df.query(
-#     "City == @city & Customer_type ==@customer_type & Gender == @gender"
-# )
-
-# ---- MAINPAGE ----
-# TOP KPI's
-# total_sales = int(df_selection["Total"].sum())
-# average_rating = round(df_selection["Rating"].mean(), 1)
-# star_rating = ":star:" * int(round(average_rating, 0))
-# average_sale_by_transaction = round(df_selection["Total"].mean(), 2)
-
-# left_column, middle_column, right_column = st.columns(3)
-# with left_column:
-#     st.subheader("Total Sales:")
-#     st.subheader(f"US $ {total_sales:,}")
-# with middle_column:
-#     st.subheader("Average Rating:")
-#     st.subheader(f"{average_rating} {star_rating}")
-# with right_column:
-#     st.subheader("Average Sales Per Transaction:")
-#     st.subheader(f"US $ {average_sale_by_transaction}")
-
-# st.markdown("""---""")
-
-# # SALES BY PRODUCT LINE [BAR CHART]
-# sales_by_product_line = (
-#     df_selection.groupby(by=["Product line"]).sum()[["Total"]].sort_values(by="Total")
-# )
-# fig_product_sales = px.bar(
-#     sales_by_product_line,
-#     x="Total",
-#     y=sales_by_product_line.index,
-#     orientation="h",
-#     title="<b>Sales by Product Line</b>",
-#     color_discrete_sequence=["#0083B8"] * len(sales_by_product_line),
-#     template="plotly_white",
-# )
-# fig_product_sales.update_layout(
-#     plot_bgcolor="rgba(0,0,0,0)",
-#     xaxis=(dict(showgrid=False))
-# )
-
-# # SALES BY HOUR [BAR CHART]
-# sales_by_hour = df_selection.groupby(by=["hour"]).sum()[["Total"]]
-# fig_hourly_sales = px.bar(
-#     sales_by_hour,
-#     x=sales_by_hour.index,
-#     y="Total",
-#     title="<b>Sales by hour</b>",
-#     color_discrete_sequence=["#0083B8"] * len(sales_by_hour),
-#     template="plotly_white",
-# )
-# fig_hourly_sales.update_layout(
-#     xaxis=dict(tickmode="linear"),
-#     plot_bgcolor="rgba(0,0,0,0)",
-#     yaxis=(dict(showgrid=False)),
-# )
-
-
-# left_column, right_column = st.columns(2)
-# left_column.plotly_chart(fig_hourly_sales, use_container_width=True)
-# right_column.plotly_chart(fig_product_sales, use_container_width=True)
-
-
 # ---- HIDE STREAMLIT STYLE ----
 hide_st_style = """
             <style>
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
             footer:after {
-                content:'Philippine Nuclear Research Institute'; 
+                content:'Made by Research Interns from National University'; 
                 visibility: visible;
                 display: block;
                 position: relative;
